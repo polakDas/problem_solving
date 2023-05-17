e0681df48d8f858f9f6ecd3ac6d6da84dabf21d3
polakD <pulok3968@gmail.com>
2023-05-18 00:59:31 +0600

    solve this

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,18 +1,31 @@
 class Solution:
     def twoSum(self, nums, target):
-        result = {}
-        for i in nums:
-            if(target-i in result):
-                print(nums.index(i))
-                return [nums.index(i), nums.index(result.get(i))]
-            else:
-                # print(nums.index(i))
-                result[i] = target - i
-        # return result
+        hashmap = {}
+        for i in range(len(nums)):
+            tempTarget = target - nums[i]
+            if tempTarget in hashmap:
+                return [i, hashmap[tempTarget]]
+            hashmap[nums[i]] = i
+            
+        # hashmap = {}
+        # for i in nums:
+        #     tempTarget = target - i
+        #     # hashmap[i] = tempTarget
+        #     if tempTarget in hashmap:
+        #         return [nums.index(tempTarget), nums.index(i)]
+        #     hashmap[i] = tempTarget
+        #     # else:
+        #         # print(nums.index(i))
+        #         # hashmap[i] = tempTarget
+        # # return result
 
 if __name__ == "__main__":
-    nums = [2,7,11,15]
-    target = 9
+    # nums = [2,7,11,15]
+    # target = 9
+    # nums = [3,2,4]
+    # target = 6
+    nums = [3,3]
+    target = 6
     
     solve = Solution()
     print(solve.twoSum(nums=nums, target=target))
