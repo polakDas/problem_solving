32fb5bd2dc685bd11f1f44449c57b1b1b75032bb
polakD <pulok3968@gmail.com>
2023-07-06 04:29:55 +0600

    help him to understand how list works

---
+++ list_comp.py
@@ -0,0 +1,34 @@
+'''
+list comprehension in python - for Rahat
+'''
+
+# fruits = ["A", "B1", "C", "D1", "E1"]
+# newlist = [x for x in fruits if "1" in x]
+# print(newlist)
+
+# nlist = []
+# for x in fruits:
+#     if "1" in x:
+#         nlist.append(x)
+# print(nlist)
+
+
+# new_list = []
+# num = [1, 2, 3, 4, 5]
+# for n in num:
+#     if n == 5:
+#         new_list.append(n)
+# print(new_list)
+
+# nums = [1,2,5,3,4,5]
+# new_nums = [num for num in nums if num == 5]
+# print(new_nums)
+
+
+# nums = [1,2,3,4,5,1,2,3,4,5]
+# 5 in nums
+
+# list, tuple, set, dictionary, string
+
+# for i in nums:
+#     print(i)
