0da6f180b82558f67682f9412567d0e06b4f3414
polakD <pulok3968@gmail.com>
2023-07-10 20:52:25 +0600

    dynamic programming

--- DP/fib_memoization.py
+++ DP/fib_memoization.py
@@ -1,14 +1,23 @@
-# It gives me factorial of 50 is almost instant!! How Rickdiculous is this!! It takes O(n) space complexity.
+# It gives me factorial of 50 almost instantly!! How Rickdiculous is this!!
+# It takes O(n) space complexity.
 # This is called memoization technique. It's a top-down approch
 
 def fib(n, lookup):
-    if n <= 1:
-        lookup[n] = n
+    '''
+    It takes a list to store the values of different factorials.
+    When a numbers factorial is stored in the lookup list, it'll return the value.
+    If we don't use this extra list, we have to calculate these values again and again
+    '''
+    if lookup[n] != None:
+        return lookup[n]
     
-    if lookup[n] is None:
-        lookup[n] = fib(n-1, lookup) + fib(n-2, lookup)
-
-    return lookup[n]
+    if n <= 2:
+        result = 1
+    else:
+        result = fib(n - 1, lookup) + fib(n - 2, lookup)
+    lookup[n] = result
+    
+    return result
 
 def main():
     n = int(input("Enter a number: "))
--- DP/fib_tabulation.py
+++ DP/fib_tabulation.py
@@ -1,14 +1,17 @@
-# This is fucking insane. It takes only O(n) time complexity.
+# This is fucking insane. It takes only O(n) time complexity for that loop.
 # It's a bottom up approach.
 
 def fib(n):
+    '''
+    Counting fib number one by one and store it to a list.
+    '''
     f = [0] * (n + 1)
 
     f[1] = 1
 
     for i in range(2, n+1):
         f[i] = f[i - 1] + f[i - 2]
-    return f[n]
+    return f
 
 def main():
     n = int(input("Enter a number: "))
