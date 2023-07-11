3b3503dbacd6537c279fb4b949b3638433a3269e
polakD <pulok3968@gmail.com>
2023-07-12 03:22:52 +0600

    sql problem solve

---
+++ leetcode/duplicate_emails.sql
@@ -0,0 +1,11 @@
+-- Create table If Not Exists Person (id int, email varchar(255))
+-- Truncate table Person
+-- insert into Person (id, email) values ('1', 'a@b.com')
+-- insert into Person (id, email) values ('2', 'c@d.com')
+-- insert into Person (id, email) values ('3', 'a@b.com')
+
+SELECT email as Email from Person
+GROUP BY email
+HAVING COUNT(email) > 1;
+
+
